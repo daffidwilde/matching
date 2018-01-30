67e7eb64952c4e43fd623e2c649030f218ab85c2
Henry Wilde <henrydavidwilde@gmail.com>
2018-01-30 21:38:33 +0000

    First commit. Add `setup.py`.

---
+++ setup.py
@@ -0,0 +1,16 @@
+""" Setup file. """
+
+
+from setuptools import setup
+
+setup(
+    name='matching',
+    version='0.1',
+    description='A package for solving matching games',
+    url='https://github.com/daffidwilde/matching',
+    author='Henry Wilde',
+    author_email='henrydavidwilde@gmail.com',
+    license='MIT',
+    keywords=['gametheory gale-shapley matchinggames'],
+    packages=['matching']
+)
