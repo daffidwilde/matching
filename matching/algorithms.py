1a5207d3d69568160a6ed161b5aef3eec1ca1dab
Henry Wilde <henrydavidwilde@gmail.com>
2018-01-30 23:52:53 +0000

    Add extended Gale-Shapley algorithm

--- matching/algorithms.py
+++ matching/algorithms.py
@@ -14,14 +14,14 @@ import numpy as np
 
 
 def galeshapley(suitor_pref_dict, reviewer_pref_dict):
-    """The Gale-Shapley algorithm. This is known to provide a unique, stable
+    """ The Gale-Shapley algorithm. This is known to provide a unique, stable
     suitor-optimal matching. The algorithm is as follows:
 
     (1) Assign all suitors and reviewers to be unmatched.
 
     (2) Take any unmatched suitor, s, and their most preferred reviewer, r.
             - If r is unmatched, match s to r.
-            - If r is matched, consider their current partner, r_partner.
+            - Else, if r is matched, consider their current partner, r_partner.
                 - If r prefers s to r_partner, unmatch r_partner from r and 
                   match s to r.
                 - Else, leave s unmatched and remove r from their preference
@@ -48,7 +48,7 @@ def galeshapley(suitor_pref_dict, reviewer_pref_dict):
     suitors = [s for s in suitor_pref_dict]
     matching = {s: None for s in suitors}
 
-    while suitors != []:
+    while suitors != [])
         s = suitors.pop(0)
         r = suitor_pref_dict[s][0]
         if r not in matching.values():
@@ -65,6 +65,83 @@ def galeshapley(suitor_pref_dict, reviewer_pref_dict):
             else:
                 suitor_pref_dict[s].remove(r)
                 suitors.append(s)
+
+    return matching
+
+
+def extended_galeshapley(suitor_pref_dict, reviewer_pref_dict, capacities):
+    """ The extended Gale-Shapley algorithm for solving a capacitated matching
+    game. This implementation of the algorithm is bas)] on that used by the NRMP
+    to solve the hospital-resident assignment problem.
+
+    The algorithm is as follows:
+
+    (1) Assign all suitors and reviewers to be unmatched.
+
+    (2) Take any unmatched suitor up for consideration, s.
+            - If their preference list is empty, remove them from consideration 
+              and go to (2).)
+            - Otherwise, consider their most preferred reviewer, r.
+            - If r currently has space for another suitor, match s to r.
+            - Otherwise, if r has no space currently:
+                - For each suitor currently matched to r, r_match:
+                    - If r prefers s to r_match and s is not yet matched to r,
+                      unmatch r_match from r and match r to s.
+                    - Otherwise, remove r from s's preference list and leave s
+                      unmatched.
+
+    (3) Go to (2) until there are no unmatched candidates up for consideration,
+        then end.
+        
+    NB: This implementation requires all reviewers to have ranked all suitors,
+        but not the other way around. That is, some suitors may end up without
+        any matches.
+
+    Parameters
+    ----------
+    suitor_pref_dict : dict
+        A dictionary with suitors as keys and their respective preference lists
+        as values
+    review_pref_dict : dict
+        A dictionary with reviewers as keys and their respective preference
+        lists as values
+    capacities : dict
+        A dictionary with reviewers as keys and their capacities as values
+
+    Returns
+    -------
+    matching : dict
+        A stable matching with reviewers as keys and lists of suitors as values
+    
+    """
+
+    free_suitors = [s for s in suitor_pref_dict]
+    matching = {r: [] for r in reviewer_pref_dict}
+
+    suitor_prefs = deepcopy(suitor_pref_dict)
+    review_prefs = deepcopy(reviewer_pref_dict)
+
+    while free_suitors:
+        s = free_suitors.pop(0)
+        s_pref = suitor_prefs[s]
+
+        if len(s_pref) > 0:
+            r = tuple(s_pref[0])
+            curr_r_matching = matching[r]
+            r_pref = reviewer_pref_dict[r]
+
+            if curr_r_matching == [] or len(curr_r_matching) < capacities[r]:
+                matching[r].append(s)
+            else:
+                for r_match in curr_r_matching:
+                    if s not in matching[r]:
+                        if r_pref.index(list(s)) < r_pref.index(list(r_match)):
+                            matching[r].remove(r_match)
+                            free_suitors.append(r_match)
+                            matching[r].append(s)
+                        else:
+                            suitor_pref_dict[s].remove(list(r))
+                            free_suitors.append(s)
 
     return matching
 
