42a785520c620ae90473b3c7481dac2179d837ba
Henry Wilde <henrydavidwilde@gmail.com>
2019-01-22 11:00:22 +0000

    "Ready for v1.0"

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@
 from setuptools import setup, find_packages
 
 with open("README.rst", "r") as readme_file:
-    readme = readme_file.read()
+    README = readme_file.read()
 
 setup(
     name="matching",
     version="1.0",
     description="A package for solving matching games.",
-    long_description=readme,
+    long_description=README,
     url="https://github.com/daffidwilde/matching",
     author="Henry Wilde",
     author_email="henrydavidwilde@gmail.com",
@@ -17,5 +17,5 @@ setup(
     keywords=["game-theory gale-shapley matching-games"],
     packages=find_packages("src"),
     package_dir={"": "src"},
-    tests_require=["pytest"],
+    tests_require=["pytest", "numpy"],
 )
--- src/matching/__init__.py
+++ src/matching/__init__.py
@@ -4,3 +4,4 @@ from .matching import Matching
 from .player import Player
 from .solver import BaseSolver
 from .solvers import StableMarriage, HospitalResident
+from .version import __version__
