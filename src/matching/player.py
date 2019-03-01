abe69ea74efbf8185fcf9e87ff2f86974fe8b6b2
Henry Wilde <henrydavidwilde@gmail.com>
2019-03-01 16:07:27 +0000

    Dump commit. Moving to basic Player class

--- src/matching/player.py
+++ src/matching/player.py
@@ -1,5 +1,4 @@
-""" A player class with methods for finding and forgetting other players. """
-
+""" Player classes for various types of player. """
 
 class Player:
     """ A class to represent a player within the matching game.
@@ -8,80 +7,38 @@ class Player:
     ==========
     name : `object`
         An identifier. This should be unique and descriptive.
-    pref_names : `list` or `tuple`
-        A list, or tuple, ranking the elements of the other set by
-        their names.
-    capacity : `int`, optional
-        The maximum number of matches the player can have at once. If not
-        specified, defaults to 1.
-
-    Attributes
-    ==========
-    matching : `Player` or `list` of `Player` instances
-        The current match(es) to the player. This is initialised as `None` and
-        will remain that way at least until the player is introduced to a game.
     """
 
-    def __init__(self, name, pref_names, capacity=1):
+    def __init__(self, name):
 
         self.name = name
-        self.pref_names = list(pref_names)
-        self._pref_names = pref_names
-        self.capacity = capacity
+        self.prefs = None
         self.matching = None
 
     def __repr__(self):
 
         return str(self.name)
 
-    def get_favourite_name(self):
-        """ Get the player's favourite name. """
+    def set_prefs(self, players):
+        """ Set the player's preferences to be an ordered list of another set of
+        `Player` instances. """
 
-        try:
-            for name in self.pref_names:
-                if name not in (match.name for match in self.matching):
-                    return name
+        self.prefs = players
 
-        except TypeError:
-            return self.pref_names[0]
+    def get_favourite(self):
+        """ Get the player's favourite member of another party. """
 
-        return None
-
-    def get_favourite(self, others):
-        """ Get the player's favourite member of the other party. """
-
-        fave_name = self.get_favourite_name()
-
-        try:
-            for other in others:
-                if other.name == fave_name and other not in self.matching:
-                    return other
-
-        except TypeError:
-            for other in others:
-                if other.name == fave_name:
-                    return other
-
-        return None
+        return self.prefs[0]
 
     def match(self, other):
         """ Assign other to be matched to the player. """
 
-        try:
-            self.matching.append(other)
-            self.matching.sort(key=lambda m: self.pref_names.index(m.name))
+        self.matching = other
 
-        except AttributeError:
-            self.matching = other
+    def unmatch(self):
+        """ Set the player to be unmatched. """
 
-    def unmatch(self, other):
-        """ Remove other from the player's current matching. """
-
-        try:
-            self.matching.remove(other)
-
-        except AttributeError:
-            self.matching = None
+        self.matching = None
 
     def get_worst_match_idx(self):
         """ Get the preference list index of the player's worst current match.
