c616807707c0b3cf9c38e000f030b6e86c617da8
Henry Wilde <henrydavidwilde@gmail.com>
2019-03-04 15:22:52 +0000

    Implement changes to SM tests.

--- tests/stable_marriage/test_algorithm.py
+++ tests/stable_marriage/test_algorithm.py
@@ -2,7 +2,7 @@
 
 from matching.solvers import stable_marriage
 
-from .params import STABLE_MARRIAGE, _make_players
+from .params import STABLE_MARRIAGE, make_players
 
 
 @STABLE_MARRIAGE
@@ -10,21 +10,18 @@ def test_suitor_optimal(player_names, seed):
     """ Verify that the suitor-oriented Gale-Shapley algorithm produces a valid,
     suitor-optimal matching for an instance of SM. """
 
-    suitor_names, reviewer_names = player_names
-    suitors, reviewers = _make_players(suitor_names, reviewer_names, seed)
+    suitors, reviewers = make_players(player_names, seed)
     matching = stable_marriage(suitors, reviewers, optimal="suitor")
 
     assert set(suitors) == set(matching.keys())
     assert set(reviewers) == set(matching.values())
 
     for suitor, reviewer in matching.items():
-        idx = suitor.pref_names.index(reviewer.name)
-        preferred = [r for r in reviewers if r.name in suitor.pref_names[:idx]]
+        idx = suitor.prefs.index(reviewer)
+        preferred = suitor.prefs[:idx]
         for rev in preferred:
             partner = rev.matching
-            assert rev.pref_names.index(suitor.name) > rev.pref_names.index(
-                partner.name
-            )
+            assert rev.prefs.index(suitor) > rev.prefs.index(partner)
 
 
 @STABLE_MARRIAGE
@@ -32,18 +29,15 @@ def test_reviewer_optimal(player_names, seed):
     """ Verify that the reviewer-oriented Gale-Shapley algorithm produces a
     valid, reviewer-optimal matching for an instance of SM. """
 
-    suitor_names, reviewer_names = player_names
-    suitors, reviewers = _make_players(suitor_names, reviewer_names, seed)
+    suitors, reviewers = make_players(player_names, seed)
     matching = stable_marriage(suitors, reviewers, optimal="reviewer")
 
     assert set(suitors) == set(matching.keys())
     assert set(reviewers) == set(matching.values())
 
     for suitor, reviewer in matching.items():
-        idx = reviewer.pref_names.index(suitor.name)
-        preferred = [s for s in suitors if s.name in reviewer.pref_names[:idx]]
-        for sui in preferred:
-            partner = sui.matching
-            assert sui.pref_names.index(reviewer.name) > sui.pref_names.index(
-                partner.name
-            )
+        idx = reviewer.prefs.index(suitor)
+        preferred = reviewer.prefs[:idx]
+        for suit in preferred:
+            partner = suit.matching
+            assert suit.prefs.index(reviewer) > suit.prefs.index(partner)
