3d97d71b4ed34e7d75d98a175bb0018e425f5f25
Henry Wilde <henrydavidwilde@gmail.com>
2019-03-04 11:49:02 +0000

    Create the Hospital class.

--- src/matching/__init__.py
+++ src/matching/__init__.py
@@ -1,7 +1,7 @@
 """ Top-level imports only. """
 
 from .matching import Matching
-from .player import Player
+from .player import Hospital, Player
 from .game import Game
 from .solvers import HospitalResident, StableMarriage, StudentAllocation
 from .version import __version__
--- src/matching/player.py
+++ src/matching/player.py
@@ -7,6 +7,16 @@ class Player:
     ==========
     name : `object`
         An identifier. This should be unique and descriptive.
+
+    Attributes
+    ==========
+    prefs : `list`
+        A list of `Player` instances in order of the player's preferences.
+        Defaults to `None` and is updated using the `set_prefs` method.
+    pref_names : `list`
+        A list the names in `prefs`. Updates with `prefs`.
+    matching : `Player` or `None`
+        The current match of the player. `None` if not currently matched.
     """
 
     def __init__(self, name):
@@ -49,20 +59,11 @@ class Player:
         prefs.remove(other)
         self.prefs = prefs
 
-    def get_match_idx(self):
-        """ Return the preference index of the player's current match. """
-
-        return self.prefs.index(self.matching)
-
-    def get_successors(self, others):
+    def get_successors(self):
         """ Get all the successors to the current match of the player. """
 
-        idx = self.get_match_idx()
-        return [
-            other
-            for other in others
-            if other in self.prefs[idx + 1 :]
-        ]
+        idx = self.prefs.index(self.matching)
+        return self.prefs[idx + 1 :]
 
     def prefers(self, player, other):
         """ Determines whether the player prefers a player over some other
@@ -70,3 +71,69 @@ class Player:
 
         prefs = self.pref_names
         return prefs.index(player.name) < prefs.index(other.name)
+
+
+class Hospital(Player):
+    """ A class to represent a hospital in an instance of HR.
+
+    Parameters
+    ==========
+    name : `object`
+        An identifier. This should be unique and descriptive.
+    capacity : `int`
+        The maximum number of matches the hospital can have.
+
+    Attributes
+    ==========
+    prefs : `list`
+        A list of `Player` instances in order of the hospital's preferences.
+        Defaults to `None` and is updated using the `set_prefs` method.
+    pref_names : `list`
+        A list the names in `prefs`. Updates with `prefs`.
+    matching : `list`
+        The current matches of the hospital. An empty list if currently
+        unsubscribed.
+    """
+
+    def __init__(self, name, capacity):
+
+        self.name = name
+        self.capacity = capacity
+        self.prefs = None
+        self.pref_names = None
+        self.matching = []
+
+    def get_favourite(self):
+        """ Get the favourite player who is not currently in the player's
+        matching. """
+
+        for player in self.prefs:
+            if player not in self.matching:
+                return player
+
+        return None
+
+    def match(self, other):
+        """ Add another player to the player's matching, and then sort it. """
+
+        self.matching.append(other)
+        self.matching.sort(key=self.prefs.index)
+
+    def unmatch(self, other):
+        """ Remove another player from the player's matching. """
+
+        matching = list(self.matching)
+        matching.remove(other)
+        self.matching = matching
+
+    def get_worst_match(self):
+        """ Get the player's worst current match. Assumes that the matching is
+        in order of preference. """
+
+        return self.matching[-1]
+
+    def get_successors(self):
+        """ Get the successors to the player's worst current match. """
+
+        idx = self.prefs.index(self.get_worst_match())
+        return self.prefs[idx + 1 :]
