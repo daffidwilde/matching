6fdb83dfa0fab60ca27dd530082a1b12412be0dd
Henry Wilde <henrydavidwilde@gmail.com>
2019-09-30 15:59:23 +0100

    Reformat players' docstrings.

--- src/matching/players/project.py
+++ src/matching/players/project.py
@@ -7,22 +7,22 @@ class Project(Hospital):
     """ A class to represent a project in an instance of SA.
 
     Parameters
-    ==========
-    name : `object`
+    ----------
+    name : object
         An identifier. This should be unique and descriptive.
-    capacity : `int`
+    capacity : int
         The maximum number of matches the project can have.
 
     Attributes
-    ==========
-    supervisor : `Supervisor`
-        The Supervisor player that runs the project. Defaults to `None`.
-    prefs : `list`
-        A list of `Player` instances in the order of the project's supervisor
-        preferences.
-    pref_names : `list`
-        A list of the names in `prefs`. Updates with `prefs`.
-    matching : `list`
+    ----------
+    supervisor : Supervisor
+        The supervisor that runs the project. Defaults to ``None``.
+    prefs : list of Player
+        The project's preferences. Inherited from ``supervisor``.
+    pref_names : list
+        A list of the names in ``prefs``. Updates with ``prefs`` via the
+        ``supervisor.set_prefs`` method.
+    matching : list of Player
         The current matches of the project. An empty list if currently
         unsubscribed.
     """
@@ -33,24 +33,24 @@ class Project(Hospital):
         self.supervisor = None
 
     def set_supervisor(self, supervisor):
-        """ Set the project's supervisor member, add the project to their list
-        of active projects and increment their capacity. """
+        """ Set the project's supervisor and add the project to their list
+        of active projects. """
 
         self.supervisor = supervisor
         if self not in supervisor.projects:
             supervisor.projects.append(self)
 
     def match(self, student):
-        """ Match the project to a student, and update the project's supervisor's
-        matching, too. """
+        """ Match the project to ``student``, and update the project
+        supervisor's matching to include ``student``, too. """
 
         self.matching.append(student)
         self.matching.sort(key=self.prefs.index)
         self.supervisor.match(student)
 
     def unmatch(self, student):
-        """ Break the matching between a project and some student, and the
-        matching between them and the supervisor member. """
+        """ Break the matching between the project and ``student``, and the
+        matching between ``student`` and the project supervisor. """
 
         matching = self.matching[:]
         matching.remove(student)
@@ -58,8 +58,8 @@ class Project(Hospital):
         self.supervisor.unmatch(student)
 
     def forget(self, student):
-        """ Remove a student from the preference list of the project and its
-        supervisor member. """
+        """ Remove ``student`` from the preference list of the project and its
+        supervisor. """
 
         prefs = self.prefs[:]
         prefs.remove(student)
