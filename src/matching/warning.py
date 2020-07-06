e44ba9691d57fb1118219fd09759cbf5bdc652f1
Henry Wilde <henrydavidwilde@gmail.com>
2020-07-06 02:49:38 +0100

    Format codebase.

--- src/matching/warning.py
+++ src/matching/warning.py
@@ -4,8 +4,10 @@
 class InvalidPreferencesWarning(UserWarning):
     """ A warning for when the preferences of a player are invalid. """
 
+
 class InvalidCapacityWarning(UserWarning):
     """ A warning for when the capacity of a player is invalid. """
+
 
 class PlayerExcludedWarning(UserWarning):
     """ A warning for when a player is to be excluded from a game. """
