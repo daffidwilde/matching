ba43799e0bdf338d60e918121d3c69ead5472a47
Henry Wilde <henrydavidwilde@gmail.com>
2020-11-03 17:48:11 +0000

    Implement extended algorithms (from book) (#130)
    * Implement extended algorithms (from book)
    
    Thanks to The Good Book (citation in `stable_roommates.py`), some minor
    adjustments to HR and SM (nothing functionally different), and a
    complete overhaul of SR have been implemented.
    
    * Fix formatting.
    
    * Add GusfieldIrving citation to bibliography.

--- src/matching/algorithms/stable_roommates.py
+++ src/matching/algorithms/stable_roommates.py
@@ -3,48 +3,28 @@
 from .util import _delete_pair
 
 
-def forget_successors(players):
-    """Make each player forget those players that they like less than their
-    current proposal."""
-
-    for player in players:
-        if player.matching:
-            successors = player.get_successors()
-            for successor in successors:
-                _delete_pair(player, successor)
-
-    return players
-
-
 def first_phase(players):
     """Conduct the first phase of the algorithm where one-way proposals are
-    made, and unpreferable pairs are forgotten. This phase terminates when
-    either all players have been proposed to, or if one player has been rejected
-    by everyone leaving their preference list empty."""
+    made, and unpreferable pairs are forgotten."""
 
-    proposed_to = set()
-    for player in players:
-        proposer = player
-        while True:
-            fave = proposer.get_favourite()
-            if not fave.matching:
-                fave._match(proposer)
-            else:
-                current = fave.matching
-                if fave.prefers(proposer, current):
-                    fave._match(proposer)
-                    _delete_pair(fave, current)
+    free_players = players[:]
+    while free_players:
 
-                    proposer = current
-                else:
-                    _delete_pair(fave, proposer)
+        player = free_players.pop()
+        favourite = player.get_favourite()
 
-            if fave not in proposed_to or not proposer.prefs:
-                break
+        current = favourite.matching
+        if current is not None:
+            favourite._unmatch()
+            free_players.append(current)
 
-        proposed_to.add(fave)
+        favourite._match(player)
 
-    players = forget_successors(players)
+        for successor in favourite.get_successors():
+            _delete_pair(successor, favourite)
+            if not successor.prefs and successor in free_players:
+                free_players.remove(successor)
+
     return players
 
 
@@ -66,34 +46,61 @@ def locate_all_or_nothing_cycle(player):
             break
 
     idx = lasts.index(player)
-    cycle = zip(lasts[idx + 1 :], seconds[idx:])
+    cycle = list(zip(lasts[idx + 1 :], seconds[idx:]))
 
     return cycle
 
 
+def get_pairs_to_delete(cycle):
+    """Based on an all-or-nothing cycle :math:`(x_1, y_1), \\ldots, (x_n, y_n)`,
+    for each :math:`i = 1, \\ldots, n`, one must delete from the game all pairs
+    :math:`(y_i, z)` such that :math:`y_i` prefers :math:`x_{i-1}` to :math:`z`
+    where subscripts are taken modulo :math:`n`.
+
+    This is an important point that is omitted from the original paper, but may
+    be found in :cite:`GI89` (Section 4.2.3).
+
+    The essential difference between this statement and that in :cite:`Irv85` is
+    the removal of unpreferable pairs, identified using an all-or-nothing cycle,
+    in addition to those contained in the cycle. Without doing so, tails of
+    cycles can be removed rather than whole cycles, leaving some conflicting
+    pairs in the game."""
+
+    pairs = []
+    for i, (_, right) in enumerate(cycle):
+
+        left = cycle[(i - 1) % len(cycle)][0]
+        successors = right.prefs[right.prefs.index(left) + 1 :]
+        for successor in successors:
+            pair = (right, successor)
+            if pair not in pairs and pair[::-1] not in pairs:
+                pairs.append((right, successor))
+
+    return pairs
+
+
 def second_phase(players):
-    """Conduct the second phase of the algorithm where all or nothing cycles
-    (rotations) are located and removed from the game. These reduced preference
-    lists form a matching."""
+    """Conduct the second phase of the algorithm where all-or-nothing cycles
+    (rotations) are located and removed from the game."""
 
-    for player in players:
-        player._unmatch()
-
-    player_with_second_preference = next(p for p in players if len(p.prefs) > 1)
+    player = next(p for p in players if len(p.prefs) > 1)
     while True:
-        cycle = locate_all_or_nothing_cycle(player_with_second_preference)
-        for player, other in cycle:
-            player._forget(other)
-            other._forget(player)
+
+        cycle = locate_all_or_nothing_cycle(player)
+        pairs = get_pairs_to_delete(cycle)
+        for player, other in pairs:
+            _delete_pair(player, other)
+
+        if any(p.prefs == [] for p in players):
+            break
 
         try:
-            player_with_second_preference = next(
-                p for p in players if len(p.prefs) > 1
-            )
+            player = next(p for p in players if len(p.prefs) > 1)
         except StopIteration:
             break
 
     for player in players:
+        player._unmatch()
         if player.prefs:
             player._match(player.get_favourite())
 
