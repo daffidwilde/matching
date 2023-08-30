b7f6167ac6db43581bfeceb94479f9e89ee8c749
Henry Wilde <henrydavidwilde@gmail.com>
2023-08-30 18:20:27 +0100

    Built site for gh-pages

--- src/matching/players/__init__.py
+++
@@ -1,8 +0,0 @@
-"""Top-level imports for the `matching.players` subpackage."""
-
-from .hospital import Hospital
-from .player import Player
-from .project import Project
-from .supervisor import Supervisor
-
-__all__ = ["Hospital", "Player", "Project", "Supervisor"]
