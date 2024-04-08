438b62be533fba4a2c7a7fcc5fe3b47673c64569
Henry Wilde <henrydavidwilde@gmail.com>
2024-04-08 16:22:20 +0100

    Translate SM algorithm test to rankings

--- tests/stable_marriage/test_algorithm.py
+++ tests/stable_marriage/test_algorithm.py
@@ -1,41 +1,33 @@
 """Integration tests for the Stable Marriage Problem algorithm."""
 
-from matching.algorithms import stable_marriage
+import numpy as np
+from hypothesis import given
 
-from .util import STABLE_MARRIAGE, make_players
-
-
-@STABLE_MARRIAGE
-def test_suitor_optimal(player_names, seed):
-    """Test that the suitor-optimal algorithm is suitor-optimal."""
-
-    suitors, reviewers = make_players(player_names, seed)
-    matching = stable_marriage(suitors, reviewers, optimal="suitor")
-
-    assert set(suitors) == set(matching.keys())
-    assert set(reviewers) == set(matching.values())
-
-    for suitor, reviewer in matching.items():
-        idx = suitor.prefs.index(reviewer)
-        preferred = suitor.prefs[:idx]
-        for rev in preferred:
-            partner = rev.matching
-            assert rev.prefs.index(suitor) > rev.prefs.index(partner)
+from .strategies import mocked_game, st_ranks
 
 
-@STABLE_MARRIAGE
-def test_reviewer_optimal(player_names, seed):
-    """Test that the reviewer-optimal algorithm is reviewer-optimal."""
+@given(st_ranks())
+def test_stable_marriage(ranks):
+    """Test that the SM algorithm is valid and suitor-optimal."""
 
-    suitors, reviewers = make_players(player_names, seed)
-    matching = stable_marriage(suitors, reviewers, optimal="reviewer")
+    suitor_ranks, reviewer_ranks = ranks
+    game = mocked_game(*ranks)
 
-    assert set(suitors) == set(matching.keys())
-    assert set(reviewers) == set(matching.values())
+    matching = game._stable_marriage(
+        suitor_ranks.copy(), reviewer_ranks.copy()
+    )
 
-    for suitor, reviewer in matching.items():
-        idx = reviewer.prefs.index(suitor)
-        preferred = reviewer.prefs[:idx]
-        for suit in preferred:
-            partner = suit.matching
-            assert suit.prefs.index(reviewer) > suit.prefs.index(partner)
+    assert isinstance(matching, dict)
+
+    assert (np.sort(list(matching.keys())) == np.unique(suitor_ranks)).all()
+    assert (
+        np.sort(list(matching.values())) == np.unique(reviewer_ranks)
+    ).all()
+
+    for reviewer, suitor in matching.items():
+        suitor_rank = suitor_ranks[suitor]
+        preferred_reviewers, *_ = np.where(suitor_rank < suitor_rank[reviewer])
+        for preferred in preferred_reviewers:
+            preferred_rank = reviewer_ranks[preferred]
+            partner = matching[preferred]
+            assert preferred_rank[suitor] > preferred_rank[partner]
