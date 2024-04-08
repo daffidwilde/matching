3afbf5f5c342d7932335fff4628052bae6e3413e
Henry Wilde <henrydavidwilde@gmail.com>
2024-04-08 08:59:51 +0100

    Implement input validator

--- src/matching/games/stable_marriage.py
+++ src/matching/games/stable_marriage.py
@@ -1,5 +1,9 @@
 """The SM game class and supporting functions."""
 
+import warnings
+
+import numpy as np
+
 from matching import convert
 
 
@@ -93,3 +97,74 @@ class StableMarriage:
         reviewer_ranks = convert.preference_to_rank(reviewer_prefs, suitors)
 
         return cls(suitor_ranks, reviewer_ranks)
+
+    def _check_number_of_players(self):
+        """
+        Check whether the player sets are the same size.
+
+        Warns
+        -----
+        UserWarning
+            If the sizes of the player sets do not match.
+        """
+
+        ns, nr = self.num_suitors, self.num_reviewers
+        if ns != nr:
+            warnings.warn(
+                f"Number of suitors ({ns}) and reviewers ({nr}) "
+                "do not match. Your matching will not be truly stable.",
+                UserWarning,
+            )
+
+    def _check_player_ranks(self, player, ranks, side):
+        """
+        Check that a player has made a strict ranking of the other side.
+
+        Parameters
+        ----------
+        player : int
+            Player to check.
+        ranks : np.ndarray
+            The player's ranking.
+        side : str
+            Name of the side to which the player belongs.
+
+        Warns
+        -----
+        UserWarning
+            If the player has not made a strict and unique ranking of
+            the other side in the game.
+        """
+
+        others = "suitors" if side == "reviewer" else "reviewers"
+        num_others = getattr(self, f"num_{others}")
+        if not np.array_equal(np.sort(ranks), np.arange(num_others)):
+            warnings.warn(
+                f"{side.title()} {player} has not strictly ranked "
+                f"all {num_others} {others}: {ranks}. "
+                "You may not be able to find a stable matching.",
+                UserWarning,
+            )
+
+    def check_input_validity(self):
+        """
+        Determine whether this game instance is valid or not.
+
+        Invalid games can still be solved, but the matching will not be
+        truly stable in the absence of blocking pairs.
+
+        Warns
+        -----
+        UserWarning
+            If (a) the player sets are not the same size; or (b) any
+            player has not made a strict, exhaustive, and unique ranking
+            of the players on the other side of the matching.
+        """
+
+        self._check_number_of_players()
+
+        for suitor, ranks in enumerate(self.suitor_ranks):
+            self._check_player_ranks(suitor, ranks, "suitor")
+
+        for reviewer, ranks in enumerate(self.reviewer_ranks):
+            self._check_player_ranks(reviewer, ranks, "reviewer")
